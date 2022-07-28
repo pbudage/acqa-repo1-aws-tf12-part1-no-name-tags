2b417f57d4b10e2b9005711fd48db7f7e043471c
AC10QA <92502365+ac10qa@users.noreply.github.com>
2022-07-28 08:53:21 +0530

    Update acqa-repo1-aws-tf12-part1.tf

--- acqa-repo1-aws-tf12-part1.tf
+++ acqa-repo1-aws-tf12-part1.tf
@@ -1,6 +1,9 @@
 provider "aws" {
   region = "ca-central-1"
-
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
 }
 
 # Create a security group with most of the vulnerabilities
