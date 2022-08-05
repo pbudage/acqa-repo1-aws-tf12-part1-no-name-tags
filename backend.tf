0d5fc811fc62be8e5b459e1c9746abc71d23542b
AC10QA <92502365+ac10qa@users.noreply.github.com>
2022-08-05 15:09:31 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tcs-engineering-tfstatestore-bucket-5362"
-    key    = "tfstates"
+    key    = "tfstates/acqa-repo1-aws-tf12-part1-no-name.tfstate"
     region = "ca-central-1"
   }
 }
